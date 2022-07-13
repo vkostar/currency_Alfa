f188cc2c300fb4abc57343864ce414add338c043
”konstantin <vihroff@gmail.com>
2022-07-13 16:27:01 +0300

    Initial commit

---
+++ src/test/java/com/example/currencyalfa/currency_alfa/CurrencyAlfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencyalfa.currency_alfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyAlfaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
