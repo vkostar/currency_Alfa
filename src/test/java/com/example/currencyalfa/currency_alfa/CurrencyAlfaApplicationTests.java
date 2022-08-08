c456c5d7a17cddf4c4427276b21311ff942c8236
”konstantin <vihroff@gmail.com>
2022-08-08 22:24:19 +0300

    completed

--- src/test/java/com/example/currencyalfa/currency_alfa/CurrencyAlfaApplicationTests.java
+++ src/test/java/com/example/currencyalfa/currency_alfa/CurrencyAlfaApplicationTests.java
@@ -1,14 +1,19 @@
 package com.example.currencyalfa.currency_alfa;
 
-import com.example.currencyalfa.currency_alfa.client.RatesClient;
 import com.example.currencyalfa.currency_alfa.models.Rates;
+import com.example.currencyalfa.currency_alfa.services.ServiceMain;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Objects;
+
 
 @SpringBootTest
 class CurrencyAlfaApplicationTests {
@@ -16,7 +21,8 @@ class CurrencyAlfaApplicationTests {
     @MockBean
     private com.example.currencyalfa.currency_alfa.client.RatesClient RatesClient;
 
-
+    @Autowired
+    ServiceMain serviceMain;
 
     @Test
     void testExchangeRates() {
@@ -38,24 +44,24 @@ class CurrencyAlfaApplicationTests {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         long currentTime = System.currentTimeMillis();
         Date todayDate = new Date(currentTime);
-        Date yesterdayDate = new Date(currentTime-24*60*60*1000);
-        String todayFormat = sdf.format(todayDate);
+        Date yesterdayDate = new Date(currentTime - 24 * 60 * 60 * 1000);
         String yesterdayFormat = sdf.format(yesterdayDate);
 
-        Mockito.when(exchangeRatesClient.getExchangeRates(Mockito.eq(todayFormat), Mockito.anyString()))
+        Mockito.when(RatesClient.getRates(Mockito.anyString()))
                 .thenReturn(todayRates);
-        Mockito.when(exchangeRatesClient.getExchangeRates(Mockito.eq(yesterdayFormat), Mockito.anyString()))
+        Mockito.when(RatesClient.getHistorical(Mockito.eq(yesterdayFormat), Mockito.anyString()))
                 .thenReturn(yesterdayRates);
+        Assert.assertTrue(serviceMain.getCurrentRates().get("CURR1")
+                > serviceMain.getHistoricRates(yesterdayFormat).get("CURR1"));
 
-        Assert.assertTrue(
-                exchangeRatesService.getRates(todayDate, "CURR1")>
-                        exchangeRatesService.getRates(yesterdayDate, "CURR1"));
-        Assert.assertTrue(
-                exchangeRatesService.getRates(todayDate, "CURR2")<
-                        exchangeRatesService.getRates(yesterdayDate, "CURR2"));
-        Assert.assertTrue(
-                exchangeRatesService.getRates(todayDate, "CURR3")==
-                        exchangeRatesService.getRates(yesterdayDate, "CURR3"));
+//        Assert.assertTrue(
+//                RatesClient.getRates("test").getRates().get("CURR1")  >
+//                        RatesClient.getHistorical(yesterdayFormat,"test").getRates().get("CURR1"));
+
+        Assert.assertTrue(serviceMain.getCurrentRates().get("CURR2")
+               < serviceMain.getHistoricRates(yesterdayFormat).get("CURR2"));
+
+        Assert.assertTrue(Objects.equals(serviceMain.getCurrentRates().get("CURR3"), serviceMain.getHistoricRates(yesterdayFormat).get("CURR3")));
 
     }
 
